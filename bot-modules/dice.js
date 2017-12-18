53efc09f3f211100fb1e2b808d82523aa9de1658
Victor Veytsman <vityavv@gmail.com>
2017-12-18 17:04:49 -0500

    Since I can't get dice to work, I'll set it aside and pick it up later

--- bot-modules/dice.js
+++ bot-modules/dice.js
@@ -2,14 +2,12 @@ module.exports = function(message, substring) {
 	var files = [];
 	var total = 0;
 	var number = Number(message.content.substring(substring)) || 1;
-	console.log(number);
 	for (var i = 0; i <= number; i++) {
 		var result = Math.floor(Math.random()*6);
 		total += result;
 		files.push(["images/dice1.png", "images/dice2.png", "images/dice3.png", "images/dice4.png", "images/dice5.png", "images/dice6.png"][result]);
-		console.log(number);
 	}
 	console.log(files);
 	message.channel.send({files});
-	message.channel.send(`Total: ${total}`);
+	message.channel.send(`Total: ${total}. WARNING: The dice function is broken. The total is correct, but not all of the dice images will show up`);
 }
