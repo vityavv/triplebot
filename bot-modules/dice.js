d6942aed2be209242b57c571e0b0fda23b6fb7a3
Victor Veytsman <vityavv@gmail.com>
2017-12-20 18:32:49 -0500

    Make sure the user doesn't break the program (fix the fact that you can only have 10 images)

--- bot-modules/dice.js
+++ bot-modules/dice.js
@@ -2,6 +2,10 @@ module.exports = function(message, substring) {
 	var files = [];
 	var total = 0;
 	var number = Number(message.content.substring(substring)) || 1;
+	if (number > 10) {
+		message.channel.send("My hand can't hold that many dice! Try something less than 11");
+		return;
+	}
 	message.channel.send(`Rolling ${number} dice`);
 	for (var i = 0; i < number; i++) {
 		var result = Math.floor(Math.random()*6);
