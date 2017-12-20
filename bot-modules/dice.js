9a556887da2fdc1ec3295c2533cc75c77c6e92c1
Victor Veytsman <vityavv@gmail.com>
2017-12-20 18:30:02 -0500

    Finally! I fixed the dice function.

--- bot-modules/dice.js
+++ bot-modules/dice.js
@@ -2,12 +2,14 @@ module.exports = function(message, substring) {
 	var files = [];
 	var total = 0;
 	var number = Number(message.content.substring(substring)) || 1;
-	for (var i = 0; i <= number; i++) {
+	message.channel.send(`Rolling ${number} dice`);
+	for (var i = 0; i < number; i++) {
 		var result = Math.floor(Math.random()*6);
-		total += result;
+		total += result + 1;
+		console.log(`Result: ${result}. Total: ${total}`);
 		files.push(["images/dice1.png", "images/dice2.png", "images/dice3.png", "images/dice4.png", "images/dice5.png", "images/dice6.png"][result]);
 	}
-	console.log(files);
+	files = files.map((file, index) => ({ attachment: file, name: `roll${index}.png` }));
 	message.channel.send({files});
-	message.channel.send(`Total: ${total}. WARNING: The dice function is broken. The total is correct, but not all of the dice images will show up`);
+	message.channel.send(`Total: ${total}.`);
 }
