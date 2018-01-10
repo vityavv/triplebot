5401bd11bce7999e1187aa3f9e1187aeb1451ad2
Victor Veytsman <vityavv@gmail.com>
2018-01-10 17:59:46 -0500

    remove a console.log

--- bot-modules/dice.js
+++ bot-modules/dice.js
@@ -10,7 +10,6 @@ module.exports = function(message, substring) {
 	for (var i = 0; i < number; i++) {
 		var result = Math.floor(Math.random()*6);
 		total += result + 1;
-		console.log(`Result: ${result}. Total: ${total}`);
 		files.push(["images/dice1.png", "images/dice2.png", "images/dice3.png", "images/dice4.png", "images/dice5.png", "images/dice6.png"][result]);
 	}
 	files = files.map((file, index) => ({ attachment: file, name: `roll${index}.png` }));
